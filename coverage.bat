3b429e80c9e83960d1ee5f28022e10666b61457b
dlebansais <dlebansais@gmail.com>
2019-01-22 14:32:40 +0100

    Added a batch file to run code coverage. [skip ci]

---
+++ coverage.bat
@@ -0,0 +1,24 @@
+@echo off
+
+if not exist "..\Misc-Beta-Test\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" goto error_console1
+if not exist "..\Misc-Beta-Test\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" goto error_console2
+if not exist "..\Misc-Beta-Test\Test-LargeList\bin\x64\Debug\Test-LargeList.dll" goto error_largelist
+if exist ..\Misc-Beta-Test\Test-LargeList\*.log del ..\Misc-Beta-Test\Test-LargeList\*.log
+if exist ..\Misc-Beta-Test\Test-LargeList\Coverage-LargeList_coverage.xml del ..\Misc-Beta-Test\Test-LargeList\Coverage-LargeList_coverage.xml
+"..\Misc-Beta-Test\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" -register:user -target:"..\Misc-Beta-Test\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" -targetargs:"..\Misc-Beta-Test\Test-LargeList\bin\x64\Debug\Test-LargeList.dll --trace=Debug --labels=All" -filter:"+[LargeList*]* -[Test-LargeList*]*" -output:"..\Misc-Beta-Test\Test-LargeList\Coverage-LargeList_coverage.xml" -showunvisited
+if exist ..\Misc-Beta-Test\Test-LargeList\Coverage-LargeList_coverage.xml ..\Misc-Beta-Test\packages\Codecov.1.1.1\tools\codecov -f "..\Misc-Beta-Test\Test-LargeList\Coverage-LargeList_coverage.xml" -t "4abfe066-5e77-4449-b893-73104119f505"
+goto end
+
+:error_console1
+echo ERROR: OpenCover.Console not found.
+goto end
+
+:error_console2
+echo ERROR: nunit3-console not found.
+goto end
+
+:error_largelist
+echo ERROR: Test-LargeList.dll not built.
+goto end
+
+:end
