7ba6e8a783cc464c923658c6199fc4c1d55dd9cf
dlebansais <dlebansais@gmail.com>
2019-08-13 22:01:53 +0200

    Trying to fix the travis build.

--- LargeList/Properties/AssemblyInfo.cs
+++ LargeList/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Runtime.InteropServices;
 using LargeList;
 
+foo
+
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
