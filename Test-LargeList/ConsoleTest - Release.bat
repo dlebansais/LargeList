9f558a6dfd90343e72a70ee7b1ab7dc963b7d72e
dlebansais <dlebansais@gmail.com>
2019-12-18 11:30:34 +0100

    Fixed some batch files.

--- Test-LargeList/ConsoleTest - Release.bat
+++ Test-LargeList/ConsoleTest - Release.bat
@@ -1,8 +1,8 @@
 @echo off
 
-if not exist "..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" goto error_console
+if not exist "..\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" goto error_console
 if not exist "..\Test-LargeList\bin\x64\Release\Test-LargeList.dll" goto error_largelist
-"..\packages\NUnit.ConsoleRunner.3.9.0\tools\nunit3-console.exe" --trace=Debug --labels=All "..\Test-LargeList\bin\x64\Release\Test-LargeList.dll"
+"..\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe" --trace=Debug --labels=All "..\Test-LargeList\bin\x64\Release\Test-LargeList.dll"
 goto end
 
 :error_console
